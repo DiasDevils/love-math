92a26e21654842572628582011923ea1c7355e07
Dia Ledesma <dia81818181@gmail.com>
2024-08-05 20:13:08 +0000

    added subtract and division games

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,6 +33,10 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion (num1, num2);
+    } else if (gameType === "divide") {
+        displayDivideQuestion (num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -67,58 +71,48 @@ function checkAnswer() {
  * directly from the dom, and returns the correct answer.
  */
 function calculateCorrectAnswer() {
-
     let operand1 = parseInt(document.getElementById('operand1').innerText);
     let operand2 = parseInt(document.getElementById('operand2').innerText);
     let operator = document.getElementById("operator").innerText;
-
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "divide"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
-    }
+    }}
 
-}
-
-/**
- * Gets the current score from the DOM and increments it by 1
- */
+/** * Gets the current score from the DOM and increments it by 1*/
 function incrementScore() {
-
     let oldScore = parseInt(document.getElementById("score").innerText);
-    document.getElementById("score").innerText = ++oldScore;
+    document.getElementById("score").innerText = ++oldScore;}
 
-}
-
-/**
- * Gets the current tally of incorrect answers from the DOM and increments it by 1
- */
+/** * Gets the current tally of incorrect answers from the DOM and increments it by 1 */
 function incrementWrongAnswer() {
-
     let oldScore = parseInt(document.getElementById("incorrect").innerText);
-    document.getElementById("incorrect").innerText = ++oldScore;
-    
-}
+    document.getElementById("incorrect").innerText = ++oldScore;}
 
 function displayAdditionQuestion(operand1, operand2) {
-
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "+";
-    
-}
+    document.getElementById('operator').textContent = "+";}
 
-function displaySubtractQuestion() {
-
-}
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";}
 
 function displayMultiplyQuestion(operand1, operand2) {
-
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "x";
+    document.getElementById('operator').textContent = "x";}
 
-}
+function displayDivideQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/";}
